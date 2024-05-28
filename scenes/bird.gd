e5284ce141ab9534e964c658e43811a6c77fadd7
jasminec1 <jacruz9@s.sfusd.edu>
2024-05-28 12:00:49 -0700

    Land scape and fixes

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -12,9 +12,9 @@ var direction = 1
 func _process(delta):
 	if ray_cast_right.is_colliding():
 		direction =-1
-		animated_sprite.flip_h = false
+		animated_sprite.flip_h = true
 	if ray_cast_left.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = true
+		animated_sprite.flip_h = false
 	position.x += direction * SPEED * delta
 
